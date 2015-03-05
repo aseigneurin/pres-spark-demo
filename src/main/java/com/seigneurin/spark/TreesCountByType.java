316d0ca17d6b462f8ea08b8b4b07bb67a8be4230
Alexis Seigneurin <alexis@seigneurin.com>
2015-03-05 22:48:05 +0100

    - minor improvements

--- src/main/java/com/seigneurin/spark/TreesCountByType.java
+++ src/main/java/com/seigneurin/spark/TreesCountByType.java
@@ -1,5 +1,7 @@
 package com.seigneurin.spark;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
@@ -7,15 +9,22 @@ import scala.Tuple2;
 public class TreesCountByType {
 
     public static void main(String[] args) {
-        JavaSparkContext sc = new JavaSparkContext("local", "trees");
+        SparkConf conf = new SparkConf()
+                .setAppName("trees");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
-        sc.textFile("/Users/aseigneurin/dev/spark-sandbox/data/arbresalignementparis2010.csv")
+        JavaPairRDD<String, Integer> rdd = sc
+                .textFile("/Users/aseigneurin/dev/spark-sandbox/data/arbresalignementparis2010.csv", 2)
                 .filter(line -> !line.startsWith("geom"))
                 .map(line -> line.split(";"))
                 .mapToPair(fields -> new Tuple2<String, Integer>(fields[4], 1))
                 .reduceByKey((x, y) -> x + y)
-                .sortByKey()
-                .foreach(t -> System.out.println(t._1 + " : " + t._2));
+                .coalesce(1)
+                .sortByKey();
+
+        rdd.foreach(t -> System.out.println(t._1 + " : " + t._2));
+
+        System.out.println(rdd.count());
 
         sc.close();
     }
